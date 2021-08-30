62f7c7ce88a33a75ca42aae6aaf77489d54febc4
winterandrea <andrecasero@gmail.com>
2021-08-30 17:46:40 +0200

    ff

--- components/footer.js
+++ components/footer.js
@@ -6,8 +6,9 @@ export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <Image src={ethWallet} alt="Ethereum Wallet" />
+        <div className="py-10 flex flex-col lg:flex-row items-center">
+          <Image width={100} height={100} src={ethWallet} alt="Ethereum Wallet" />
+          <h1>0xEC5F81a5477147B606C68E8C2366E7846a0E71dA</h1>
         </div>
       </Container>
     </footer>
