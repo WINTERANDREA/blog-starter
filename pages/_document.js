7f8e54165a7eb48a3be915ee6bece739e18e664d
winterandrea <andrecasero@gmail.com>
2021-08-30 18:00:00 +0200

    analytics and mailchimp

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head >
-          <Script id="mcjs" dangerouslySetInnerHTML={{
+          <script id="mcjs" dangerouslySetInnerHTML={{
               __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/f89e24801b73abf6542871f91/7c95f6c9aadc13560456b2a52.js");`}}/>
-          <Script async src={`https://www.googletagmanager.com/gtag/js?id=G-LBLMF0QDQH`}></Script>
-          <Script  dangerouslySetInnerHTML={{
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=G-LBLMF0QDQH`}/>
+          <script  dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments)}
